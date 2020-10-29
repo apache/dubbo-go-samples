2397c72020dce3bfee3bb384663fa910ccef85a8
Ian Luo <ian.luo@gmail.com>
2020-10-29 19:08:14 +0800

    make IT works for generic samples

---
+++ generic/go-server/tests/integration/main_test.go
@@ -0,0 +1,58 @@
+// +build integration
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package integration
+
+import (
+	"github.com/apache/dubbo-go/config"
+	"github.com/apache/dubbo-go/protocol/dubbo"
+)
+
+import (
+	"os"
+	"testing"
+	"time"
+)
+
+var appName = "UserConsumerTest"
+var referenceConfig = config.ReferenceConfig{
+	InterfaceName: "org.apache.dubbo.UserProvider",
+	Cluster:       "failover",
+	Registry:      "demoZk",
+	Protocol:      dubbo.DUBBO,
+	Generic:       true,
+}
+
+func TestMain(m *testing.M) {
+	referenceConfig.GenericLoad(appName)
+	time.Sleep(3 * time.Second)
+
+	os.Exit(m.Run())
+}
+
+type User struct {
+	Id   string
+	Name string
+	Age  int32
+	Time time.Time
+}
+
+func (User) JavaClassName() string {
+	return "org.apache.dubbo.User"
+}
