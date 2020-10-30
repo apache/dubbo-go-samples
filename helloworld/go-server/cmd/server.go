cdbddd8a7047648c8d645d17cbdb8fad5b1f7aab
Ian Luo <ian.luo@gmail.com>
2020-10-30 11:27:58 +0800

    clean up imports

--- helloworld/go-server/cmd/server.go
+++ helloworld/go-server/cmd/server.go
@@ -27,6 +27,7 @@ import (
 
 import (
 	hessian "github.com/apache/dubbo-go-hessian2"
+	"github.com/apache/dubbo-samples/golang/helloworld/go-server/pkg"
 
 	_ "github.com/apache/dubbo-go/cluster/cluster_impl"
 	_ "github.com/apache/dubbo-go/cluster/loadbalance"
@@ -37,8 +38,6 @@ import (
 	_ "github.com/apache/dubbo-go/protocol/dubbo"
 	_ "github.com/apache/dubbo-go/registry/protocol"
 	_ "github.com/apache/dubbo-go/registry/zookeeper"
-
-	"github.com/apache/dubbo-samples/golang/helloworld/go-server/pkg"
 )
 
 var (
