e4e67fbdd861a0d7e281112eb3d7839b0fb96d71
Joe Zou <yixian.zou@gmail.com>
2020-10-31 16:26:42 +0800

    travis add Make

---
+++ helloworld/integrate_test.sh
@@ -0,0 +1,21 @@
+#
+#  Licensed to the Apache Software Foundation (ASF) under one or more
+#  contributor license agreements.  See the NOTICE file distributed with
+#  this work for additional information regarding copyright ownership.
+#  The ASF licenses this file to You under the Apache License, Version 2.0
+#  (the "License"); you may not use this file except in compliance with
+#  the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+docker run --name zookeeper -p2181:2181 -d zookeeper
+cd helloworld/go-server
+make -f ../../build/Makefile integration
+make -f ../../build/Makefile clean
+make -f ../../build/Makefile docker-down
