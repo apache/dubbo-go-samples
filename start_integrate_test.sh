0cbfec8c0008ff12fd6096d527b072306a4db2b1
Joe Zou <yixian.zou@gmail.com>
2020-10-31 16:32:58 +0800

    delete

--- start_integrate_test.sh
+++ start_integrate_test.sh
@@ -14,4 +14,4 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-helloworld/dubbo/integrate_test.sh
+#helloworld/dubbo/integrate_test.sh
