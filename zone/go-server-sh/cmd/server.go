e98b47d744dd6fa883b892950c3113be307c9038
Ian Luo <ian.luo@gmail.com>
2020-11-02 14:11:31 +0800

    make integration test work for metric and zone

--- zone/go-server-sh/cmd/server.go
+++ zone/go-server-sh/cmd/server.go
@@ -18,7 +18,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -27,64 +26,28 @@ import (
 )
 
 import (
-	"github.com/dubbogo/gost/log"
-)
-
-import (
 	hessian "github.com/apache/dubbo-go-hessian2"
-	"github.com/apache/dubbo-go/common/constant"
-	"github.com/apache/dubbo-go/common/logger"
-	_ "github.com/apache/dubbo-go/common/proxy/proxy_factory"
-	"github.com/apache/dubbo-go/config"
-	_ "github.com/apache/dubbo-go/protocol/dubbo"
-	_ "github.com/apache/dubbo-go/registry/protocol"
-
-	_ "github.com/apache/dubbo-go/filter/filter_impl"
 
 	_ "github.com/apache/dubbo-go/cluster/cluster_impl"
 	_ "github.com/apache/dubbo-go/cluster/loadbalance"
+	"github.com/apache/dubbo-go/common/logger"
+	_ "github.com/apache/dubbo-go/common/proxy/proxy_factory"
+	"github.com/apache/dubbo-go/config"
+	_ "github.com/apache/dubbo-go/filter/filter_impl"
+	_ "github.com/apache/dubbo-go/protocol/dubbo"
+	_ "github.com/apache/dubbo-go/registry/protocol"
 	_ "github.com/apache/dubbo-go/registry/zookeeper"
+	"github.com/apache/dubbo-samples/golang/zone/go-server-sh/pkg"
 )
 
 var (
-	//survivalTimeout int = 10e9
-	survivalTimeout int = 60 * 1000
+	survivalTimeout int = 10e9
 )
 
-// they are necessary:
-// 		export CONF_CONSUMER_FILE_PATH="xxx"
-// 		export APP_LOG_CONF_FILE="xxx"
+// need to setup environment variable "CONF_PROVIDER_FILE_PATH" to "conf/server.yml" before run
 func main() {
-	hessian.RegisterPOJO(&User{})
+	hessian.RegisterPOJO(&pkg.User{})
 	config.Load()
-	time.Sleep(3e9)
-
-	gxlog.CInfo("\n\n\nstart to test dubbo")
-
-	// context zone hangzhou,
-	ctx := context.Background()
-	// if set zoneForce, must have zone tag
-	ctx = context.WithValue(ctx, constant.REGISTRY_KEY+"."+constant.ZONE_FORCE_KEY, true)
-
-	var hz, sh int
-	loop := 50
-	user := &User{}
-	for i := 0; i < loop; i++ {
-		err := userProvider.GetUser(ctx, []interface{}{i}, user)
-		if err != nil {
-			panic(err)
-		}
-		if "dev-hz" == user.Id {
-			hz++
-		}
-		if "dev-sh" == user.Id {
-			sh++
-		}
-		gxlog.CInfo("response %d result: %v\n", i, user)
-	}
-
-	gxlog.CInfo("loop count : %d, hangzhou count : %d, shanghai count : %d", loop,
-		hz, sh)
 
 	initSignal()
 }
@@ -92,8 +55,7 @@ func main() {
 func initSignal() {
 	signals := make(chan os.Signal, 1)
 	// It is not possible to block SIGKILL or syscall.SIGSTOP
-	signal.Notify(signals, os.Interrupt, os.Kill, syscall.SIGHUP,
-		syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(signals, os.Interrupt, os.Kill, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
 	for {
 		sig := <-signals
 		logger.Infof("get signal %s", sig.String())
@@ -107,7 +69,7 @@ func initSignal() {
 			})
 
 			// The program exits normally or timeout forcibly exits.
-			fmt.Println("app exit now...")
+			fmt.Println("provider app exit now...")
 			return
 		}
 	}
