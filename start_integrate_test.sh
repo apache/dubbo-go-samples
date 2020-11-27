5d585d2c412a19ef2f635d0bc3d7d99d29efcaff
Joe Zou <yixian.zou@gmail.com>
2020-11-27 22:00:56 +0800

    update test case

--- start_integrate_test.sh
+++ start_integrate_test.sh
@@ -14,5 +14,4 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-helloworld/integrate_test.sh
-general/dubbo/integrate_test.sh
+./integrate_test.sh $(pwd)/helloworld/go-server
