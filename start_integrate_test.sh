d02ac34f6b8836e97a69f59822d8eafd6fa0e94f
Joe Zou <yixian.zou@gmail.com>
2020-11-29 22:37:44 +0800

    add exit code for test

--- start_integrate_test.sh
+++ start_integrate_test.sh
@@ -15,27 +15,34 @@
 #  limitations under the License.
 
 # async
-./integrate_test.sh async/go-server
+#./integrate_test.sh async/go-server
+#
+## config center
+#./integrate_test.sh configcenter/apollo/go-server
+#./integrate_test.sh configcenter/nacos/go-server
+#
+## direct
+#./integrate_test.sh direct/go-server
+#
+## filter
+#./integrate_test.sh filter/custom_filter/go-server
+#./integrate_test.sh filter/tpslimit/go-server
 
-# config center
-./integrate_test.sh configcenter/apollo/go-server
-./integrate_test.sh configcenter/nacos/go-server
+## generic
+#./integrate_test.sh generic/dubbo/go-server
+#
+## hello world
+#./integrate_test.sh helloworld/go-server
+#
+## metric
+#./integrate_test.sh metric/go-server
 
- direct
-./integrate_test.sh direct/go-server
-
-# filter
-./integrate_test.sh filter/custom_filter/go-server
-./integrate_test.sh filter/tpslimit/go-server
-
-# general
-./integrate_test.sh general/dubbo/go-server
-
-# generic
-./integrate_test.sh generic/dubbo/go-server
-
-# hello world
-./integrate_test.sh helloworld/go-server
-
-# metric
-./integrate_test.sh metric/go-server
+array=("general/dubbo/go-server")
+#通过下标遍历
+for((i=0;i<${#array[*]};i++))
+do
+	./integrate_test.sh ${array[i]}
+	if [ $? -gt 0 ]; then
+    exit $?
+	fi
+done
