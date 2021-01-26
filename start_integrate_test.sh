d1d800a7c39887bd5a9dc8d97eccc00c50c2d7e2
LaurenceLiZhixin <382673304@qq.com>
2021-01-26 13:06:16 +0800

    add consul support

--- start_integrate_test.sh
+++ start_integrate_test.sh
@@ -15,47 +15,47 @@
 #  limitations under the License.
 
 # async
-array=("async/go-server")
+#array=("async/go-server")
 
 # config center
-array+=("configcenter/apollo/go-server")
-array+=("configcenter/nacos/go-server")
+#array+=("configcenter/apollo/go-server")
+#array+=("configcenter/nacos/go-server")
 #array+=("configcenter/zookeeper/go-server")
 
 # direct
-array+=("direct/go-server")
+#array+=("direct/go-server")
 
 # filter
-array+=("filter/custom_filter/go-server")
-array+=("filter/tpslimit/go-server")
-array+=("filter/sentinel/go-server")
+#array+=("filter/custom_filter/go-server")
+#array+=("filter/tpslimit/go-server")
+#array+=("filter/sentinel/go-server")
 
 # general
-array+=("general/dubbo/go-server")
-array+=("general/grpc/go-server")
+#array+=("general/dubbo/go-server")
+#array+=("general/grpc/go-server")
 #array+=("general/jsonrpc/go-server")
 #array+=("general/rest/go-server")
 
 # generic
-array+=("generic/go-server")
+#array+=("generic/go-server")
 
 # hello world
-array+=("helloworld/go-server")
+#array=("helloworld/go-server")
 
 # metric
-array+=("metric/go-server")
+#array+=("metric/go-server")
 
 # multi-registry
 #array+=("multi-registry/go-server")
 
 # router
-array+=("router/condition/go-server")
+#array+=("router/condition/go-server")
 #array+=("router/tag/go-server")
 
 # registry/servicediscovery/zookeeper
-array+=("registry/servicediscovery/zookeeper/go-server")
+#array+=("registry/servicediscovery/zookeeper/go-server")
 # registry/servicediscovery/consul
-#array+=("registry/servicediscovery/consul/go-server")
+array=("registry/servicediscovery/consul/go-server")
 
 
 for((i=0;i<${#array[*]};i++))
