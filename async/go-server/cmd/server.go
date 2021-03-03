556f3ab8ce2246a9763a786587225032b942f21c
Ian Luo <ian.luo@gmail.com>
2021-03-03 21:14:13 +0800

    add run configurations and fix client issue.

--- async/go-server/cmd/server.go
+++ async/go-server/cmd/server.go
@@ -27,7 +27,10 @@ import (
 
 import (
 	hessian "github.com/apache/dubbo-go-hessian2"
+	"github.com/apache/dubbo-go-samples/helloworld/go-server/pkg"
+)
 
+import (
 	_ "github.com/apache/dubbo-go/cluster/cluster_impl"
 	_ "github.com/apache/dubbo-go/cluster/loadbalance"
 	"github.com/apache/dubbo-go/common/logger"
@@ -37,8 +40,6 @@ import (
 	_ "github.com/apache/dubbo-go/protocol/dubbo"
 	_ "github.com/apache/dubbo-go/registry/protocol"
 	_ "github.com/apache/dubbo-go/registry/zookeeper"
-
-	"github.com/apache/dubbo-go-samples/helloworld/go-server/pkg"
 )
 
 var (
