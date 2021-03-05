f8e61a3c10978282f8deef1d58e723bd6e58dc40
yakecanlee <yakecanlee@gmail.com>
2021-03-05 10:26:36 +0800

    adjust imports and readme

--- group/go-server-group-a/pkg/user.go
+++ group/go-server-group-a/pkg/user.go
@@ -23,12 +23,9 @@ import (
 )
 
 import (
-	"github.com/dubbogo/gost/log"
-)
-
-import (
 	hessian "github.com/apache/dubbo-go-hessian2"
 	"github.com/apache/dubbo-go/config"
+	"github.com/dubbogo/gost/log"
 )
 
 func init() {
@@ -38,7 +35,7 @@ func init() {
 }
 
 type User struct {
-	Id   string
+	ID   string
 	Name string
 	Age  int32
 	Time time.Time
