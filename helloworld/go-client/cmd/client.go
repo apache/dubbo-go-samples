f17c5dce6af36e2abf25fd18b058b18144891d93
yakecanlee <yakecanlee@gmail.com>
2021-03-11 16:58:45 +0800

    fix helloworld sample

--- helloworld/go-client/cmd/client.go
+++ helloworld/go-client/cmd/client.go
@@ -25,7 +25,7 @@ import (
 
 import (
 	hessian "github.com/apache/dubbo-go-hessian2"
-	"github.com/apache/dubbo-go-samples/filter/custom_filter/go-client/pkg"
+	"github.com/apache/dubbo-go-samples/helloworld/go-client/pkg"
 	"github.com/dubbogo/gost/log"
 )
 
