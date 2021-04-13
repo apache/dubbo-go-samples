f3938f550a2378d4f7f4775e80e9062057cf0168
Ian Luo <ian.luo@gmail.com>
2021-04-13 16:29:44 +0800

    enhance tls examples (#91)

--- tls/go-client/cmd/client.go
+++ tls/go-client/cmd/client.go
@@ -30,17 +30,16 @@ import (
 import (
 	getty "github.com/apache/dubbo-getty"
 	hessian "github.com/apache/dubbo-go-hessian2"
-	gxlog "github.com/dubbogo/gost/log"
+	_ "github.com/apache/dubbo-go/cluster/cluster_impl"
+	_ "github.com/apache/dubbo-go/cluster/loadbalance"
 	"github.com/apache/dubbo-go/common/logger"
 	_ "github.com/apache/dubbo-go/common/proxy/proxy_factory"
 	"github.com/apache/dubbo-go/config"
+	_ "github.com/apache/dubbo-go/filter/filter_impl"
 	_ "github.com/apache/dubbo-go/protocol/dubbo"
 	_ "github.com/apache/dubbo-go/registry/protocol"
-	_ "github.com/apache/dubbo-go/filter/filter_impl"
-	_ "github.com/apache/dubbo-go/cluster/cluster_impl"
-	_ "github.com/apache/dubbo-go/cluster/loadbalance"
 	_ "github.com/apache/dubbo-go/registry/zookeeper"
-
+	gxlog "github.com/dubbogo/gost/log"
 )
 
 import (
