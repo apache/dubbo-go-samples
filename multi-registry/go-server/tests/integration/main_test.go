570c70447fca96f1681618412a961b291de2f579
Ian Luo <ian.luo@gmail.com>
2021-04-14 00:09:59 +0800

    clean up integration test (#92)
    * clean up integration test
    
    * disable game
    
    * disable general/rest
    
    * fix integration test for group
    
    * fix integration test for multi-registry
    
    * add run config for multi-registry
    
    * more run configs and README enhancement
    
    * disable multi-registry
    
    * fix build issue
    
    * increase etcd connection timeout
    
    * enhance docker-compose
    
    * disable etcd from integration test
    
    * disable registry/nacos
    
    * enhance integraton tests
    
    * enhance integration test
    
    * disable consul and etcd
    
    * disable servicediscovery/etcd
    
    * disable tls

--- multi-registry/go-server/tests/integration/main_test.go
+++ multi-registry/go-server/tests/integration/main_test.go
@@ -21,13 +21,13 @@ package integration
 
 import (
 	hessian "github.com/apache/dubbo-go-hessian2"
-	"github.com/apache/dubbo-go/config"
-
 	_ "github.com/apache/dubbo-go/cluster/cluster_impl"
 	_ "github.com/apache/dubbo-go/cluster/loadbalance"
 	_ "github.com/apache/dubbo-go/common/proxy/proxy_factory"
+	"github.com/apache/dubbo-go/config"
 	_ "github.com/apache/dubbo-go/filter/filter_impl"
 	_ "github.com/apache/dubbo-go/protocol/dubbo"
+	_ "github.com/apache/dubbo-go/registry/nacos"
 	_ "github.com/apache/dubbo-go/registry/protocol"
 	_ "github.com/apache/dubbo-go/registry/zookeeper"
 )
