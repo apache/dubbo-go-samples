b3966ddc78390da6bdb7e4456aae4b5686f64562
氕氘氚 <cjp_ca@163.com>
2021-05-11 12:24:08 +0800

    add readme of game (#97)
    add readme of game (#97)

--- game/go-server-game/pkg/consumer.go
+++ game/go-server-game/pkg/consumer.go
@@ -2,6 +2,4 @@ package pkg
 
 import "github.com/apache/dubbo-go-samples/game/pkg/consumer/gate"
 
-var gateBasketball = new(gate.BasketballService)
-
-// var gateJump = new(gate.JumpService)
+var GateBasketball = new(gate.BasketballService)
--- game/go-server-game/pkg/provider.go
+++ game/go-server-game/pkg/provider.go
@@ -2,6 +2,8 @@ package pkg
 
 import (
     "context"
+    "fmt"
+
     "github.com/apache/dubbo-go/common/logger"
 
     "github.com/apache/dubbo-go-samples/game/pkg/pojo"
@@ -11,56 +13,27 @@ type BasketballService struct{}
 
 func (p *BasketballService) Online(ctx context.Context, uid string) (*pojo.Result, error) {
     logger.Infof("online: %s", uid)
-    return &pojo.Result{Code: 0}, nil
+    return &pojo.Result{Code: 0, Msg: "hello this is game provider"}, nil
 }
 
 func (p *BasketballService) Offline(ctx context.Context, uid string) (*pojo.Result, error) {
     logger.Infof("offline: %#s", uid)
-    return &pojo.Result{Code: 0}, nil
+    return &pojo.Result{Code: 0, Msg: "hello this is game provider"}, nil
 }
 
 func (p *BasketballService) Message(ctx context.Context, uid, data string) (*pojo.Result, error) {
     logger.Infof("message: %#v, %#v", uid, data)
 
-    // // auto reply the same message
-    // _, err := gateBasketball.Send(context.TODO(), uid, data)
-    // if err != nil {
-    //     logger.Errorf("send fail: %#s", err.Error())
-    //     return &pojo.Result{Code: 1, Msg: err.Error()}, err
-    // }
-
-    return &pojo.Result{Code: 0, Data: map[string]interface{}{"to": uid, "message": data}}, nil
-}
-
-func (p *BasketballService) Reference() string {
-    return "gameProvider.basketballService"
-}
-
-type JumpService struct{}
-
-func (p *JumpService) Online(ctx context.Context, uid string) (*pojo.Result, error) {
-    logger.Infof("online: %#s", uid)
-    return &pojo.Result{Code: 0}, nil
-}
-
-func (p *JumpService) Offline(ctx context.Context, uid string) (*pojo.Result, error) {
-    logger.Infof("offline: %#s", uid)
-    return &pojo.Result{Code: 0}, nil
-}
-
-func (p *JumpService) Message(ctx context.Context, uid, data string) (*pojo.Result, error) {
-    logger.Infof("message: %#s, %#s", uid, data)
-
-    // reply the same message
-    _, err := gateBasketball.Send(context.TODO(), uid, data)
+    // auto reply the same message
+    rsp, err := GateBasketball.Send(context.TODO(), uid, data)
     if err != nil {
         logger.Errorf("send fail: %#s", err.Error())
         return &pojo.Result{Code: 1, Msg: err.Error()}, err
     }
-
-    return &pojo.Result{Code: 0, Data: map[string]interface{}{"to": uid, "message": data}}, nil
+    fmt.Println("receive data from gate:", rsp)
+    return &pojo.Result{Code: 0, Data: map[string]interface{}{"to": uid, "message": data + " from game provider"}}, nil
 }
 
-func (p *JumpService) Reference() string {
-    return "gameProvider.jumpService"
+func (p *BasketballService) Reference() string {
+    return "gameProvider.basketballService"
 }
