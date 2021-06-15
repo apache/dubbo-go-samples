cf067423869bb1bfb3a8b4805c24a50ec697be8c
LaurenceLiZhixin <382673304@qq.com>
2021-06-15 20:41:44 +0800

    fix

--- start_integrate_test.sh
+++ start_integrate_test.sh
@@ -14,65 +14,65 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-# async
-array=("async/go-server")
-
-# config center
-array+=("configcenter/apollo/go-server")
-array+=("configcenter/nacos/go-server")
-array+=("configcenter/zookeeper/go-server")
-
-# context
-array+=("context/go-server")
-
-# direct
-array+=("direct/go-server")
-
-# filter
-array+=("filter/custom/go-server")
-array+=("filter/tpslimit/go-server")
-array+=("filter/sentinel/go-server")
-
-# general-dubbo
-array+=("general/dubbo/go-server")
-
-# general-grpc
-array+=("general/grpc/go-server")
-
-# general-dubbo3(triple)
-array+=("general/dubbo3/pb/dubbogo-grpc/server/dubbogo-server")
-array+=("general/dubbo3/pb/dubbogo-java/go-server")
-array+=("general/dubbo3/hessian2/go-server")
-array+=("general/dubbo3/msgpack/go-server")
-array+=("general/dubbo3/codec-extension/go-server")
-
-array+=("generic/go-server")
-
-# group
-array+=("group/go-server-group-a")
-array+=("group/go-server-group-b")
-
-# hello world
-array+=("helloworld/go-server")
-
-# metric
-array+=("metric/go-server")
-
-# multi-registry
-# array+=("multi-registry/go-server")
-
-array+=("registry/zookeeper/go-server")
-array+=("registry/etcd/go-server")
+## async
+#array=("async/go-server")
+#
+## config center
+#array+=("configcenter/apollo/go-server")
+#array+=("configcenter/nacos/go-server")
+#array+=("configcenter/zookeeper/go-server")
+#
+## context
+#array+=("context/go-server")
+#
+## direct
+#array+=("direct/go-server")
+#
+## filter
+#array+=("filter/custom/go-server")
+#array+=("filter/tpslimit/go-server")
+#array+=("filter/sentinel/go-server")
+#
+## general-dubbo
+#array+=("general/dubbo/go-server")
+#
+## general-grpc
+#array+=("general/grpc/go-server")
+#
+## general-dubbo3(triple)
+#array+=("general/dubbo3/pb/dubbogo-grpc/server/dubbogo-server")
+#array+=("general/dubbo3/pb/dubbogo-java/go-server")
+#array+=("general/dubbo3/hessian2/go-server")
+#array+=("general/dubbo3/msgpack/go-server")
+#array+=("general/dubbo3/codec-extension/go-server")
+#
+#array+=("generic/go-server")
+#
+## group
+#array+=("group/go-server-group-a")
+#array+=("group/go-server-group-b")
+#
+## hello world
+#array+=("helloworld/go-server")
+#
+## metric
+#array+=("metric/go-server")
+#
+## multi-registry
+## array+=("multi-registry/go-server")
+#
+#array+=("registry/zookeeper/go-server")
+#array+=("registry/etcd/go-server")
 array+=("registry/nacos/go-server")
 
-# registry/servicediscovery/zookeeper
-array+=("registry/servicediscovery/zookeeper/go-server")
-array+=("registry/servicediscovery/nacos/go-server")
-
-
-# router
-array+=("router/uniform-router/file/go-server")
-array+=("router/uniform-router/file/go-server2")
+## registry/servicediscovery/zookeeper
+#array+=("registry/servicediscovery/zookeeper/go-server")
+#array+=("registry/servicediscovery/nacos/go-server")
+#
+#
+## router
+#array+=("router/uniform-router/file/go-server")
+#array+=("router/uniform-router/file/go-server2")
 
 
 for((i=0;i<${#array[*]};i++))
