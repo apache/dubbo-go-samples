419049c8ff98257578584f3434bd30eb5477f755
Laurence <45508533+LaurenceLiZhixin@users.noreply.github.com>
2021-07-13 13:18:22 +0800

    fix: fix dynamic naming mapping of app level service disc (#155)
    * fix: fix dynamic mapping
    
    * fix

--- registry/servicediscovery/nacos/go-server/cmd/server.go
+++ registry/servicediscovery/nacos/go-server/cmd/server.go
@@ -27,14 +27,18 @@ import (
 
 import (
 	hessian "github.com/apache/dubbo-go-hessian2"
+	"github.com/apache/dubbo-go-samples/registry/servicediscovery/nacos/go-server/pkg"
+)
 
+import (
 	_ "dubbo.apache.org/dubbo-go/v3/cluster/cluster_impl"
 	_ "dubbo.apache.org/dubbo-go/v3/cluster/loadbalance"
 	"dubbo.apache.org/dubbo-go/v3/common/logger"
 	_ "dubbo.apache.org/dubbo-go/v3/common/proxy/proxy_factory"
 	"dubbo.apache.org/dubbo-go/v3/config"
+	_ "dubbo.apache.org/dubbo-go/v3/config_center/nacos"
 	_ "dubbo.apache.org/dubbo-go/v3/filter/filter_impl"
-	_ "dubbo.apache.org/dubbo-go/v3/metadata/mapping/memory"
+	_ "dubbo.apache.org/dubbo-go/v3/metadata/mapping/dynamic"
 	_ "dubbo.apache.org/dubbo-go/v3/metadata/report/nacos"
 	_ "dubbo.apache.org/dubbo-go/v3/metadata/service/remote"
 	_ "dubbo.apache.org/dubbo-go/v3/protocol/dubbo"
@@ -42,10 +46,6 @@ import (
 	_ "dubbo.apache.org/dubbo-go/v3/registry/nacos"
 	_ "dubbo.apache.org/dubbo-go/v3/registry/protocol"
 	_ "dubbo.apache.org/dubbo-go/v3/registry/servicediscovery"
-)
-
-import (
-	"github.com/apache/dubbo-go-samples/registry/servicediscovery/nacos/go-server/pkg"
 )
 
 var (
