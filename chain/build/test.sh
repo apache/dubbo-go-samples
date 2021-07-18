0277b53e3d7c8a0a134937edaee2f635dea77a61
LaurenceLiZhixin <382673304@qq.com>
2021-07-18 09:23:01 +0800

    fix: add waiter after middle start

--- chain/build/test.sh
+++ chain/build/test.sh
@@ -26,6 +26,7 @@ make PROJECT_DIR="$P_DIR" PROJECT_NAME="$(basename "$P_DIR")" BASE_DIR="$P_DIR"/
 make PROJECT_DIR="$P_DIR"/backend PROJECT_NAME="$(basename "$P_DIR"/backend)" BASE_DIR="$P_DIR"/backend/dist -f build/Makefile start
 sleep 10
 make PROJECT_DIR="$P_DIR"/middle PROJECT_NAME="$(basename "$P_DIR"/middle)" CONF_CONSUMER_FILE_PATH="$P_DIR"/middle/conf/client.yml BASE_DIR="$P_DIR"/middle/dist -f build/Makefile start
+sleep 10
 
 # start integration testing
 make PROJECT_DIR="$P_DIR"/backend PROJECT_NAME="$(basename "$P_DIR"/backend)" BASE_DIR="$P_DIR"/backend/dist -f build/Makefile integration
