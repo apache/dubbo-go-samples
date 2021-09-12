c86fba40d088d773084511dabf6a4a36118382f9
luky116 <luky116@126.com>
2021-09-12 22:49:40 +0800

    add jsonrpc tracing sample

---
+++ tracing/jsonrpc/go-client/cmd/client.go
@@ -0,0 +1,124 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"dubbo.apache.org/dubbo-go/v3/config"
+	"fmt"
+	gxlog "github.com/dubbogo/gost/log"
+	"github.com/opentracing/opentracing-go"
+	zipkinot "github.com/openzipkin-contrib/zipkin-go-opentracing"
+	"github.com/openzipkin/zipkin-go"
+	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
+	"github.com/uber/jaeger-client-go"
+	jaegerConfig "github.com/uber/jaeger-client-go/config"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+import (
+	"dubbo.apache.org/dubbo-go/v3/common/logger"
+	_ "dubbo.apache.org/dubbo-go/v3/imports"
+)
+
+import (
+	"github.com/apache/dubbo-go-samples/tracing/jsonrpc/go-client/pkg"
+)
+
+var (
+	survivalTimeout int = 10e9
+	userProvider        = new(pkg.UserProvider)
+)
+
+func main() {
+	config.SetConsumerService(userProvider)
+	config.Load(config.WithPath("./tracing/jsonrpc/go-client/conf/dubbogo.yml"))
+	// initJaeger() and initZipkin() can only use one at the same time
+	initJaeger()
+	//initZipkin()
+	gxlog.CInfo("start to test jsonrpc")
+	span, spanCtx := opentracing.StartSpanFromContext(context.Background(), "Dubbogo-Client-Service")
+	user, err := userProvider.GetUser(spanCtx, []interface{}{"A003"})
+	span.Finish()
+	if err != nil {
+		gxlog.CInfo("response result: %v", err)
+		panic(err)
+	}
+	gxlog.CInfo("response result: %v", user)
+	initSignal()
+}
+
+func initSignal() {
+	signals := make(chan os.Signal, 1)
+	// It is not possible to block SIGKILL or syscall.SIGSTOP
+	signal.Notify(signals, os.Interrupt, os.Kill, syscall.SIGHUP,
+		syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		sig := <-signals
+		logger.Infof("get signal %s", sig.String())
+		switch sig {
+		case syscall.SIGHUP:
+			// reload()
+		default:
+			time.AfterFunc(time.Duration(survivalTimeout), func() {
+				logger.Warnf("app exit now by force...")
+				os.Exit(1)
+			})
+			// The program exits normally or timeout forcibly exits.
+			fmt.Println("app exit now...")
+			return
+		}
+	}
+}
+
+func initJaeger() {
+	cfg := jaegerConfig.Configuration{
+		ServiceName: "dobbugoJaegerTracingService",
+		Sampler: &jaegerConfig.SamplerConfig{
+			Type:  jaeger.SamplerTypeRemote,
+			Param: 1,
+		},
+		Reporter: &jaegerConfig.ReporterConfig{
+			LocalAgentHostPort:  "127.0.0.1:6831",
+			LogSpans:            true,
+			BufferFlushInterval: 5 * time.Second,
+		},
+	}
+	nativeTracer, _, err := cfg.NewTracer(jaegerConfig.Logger(jaeger.StdLogger))
+	if err != nil {
+		logger.Errorf("unable to create jaeger tracer: %+v\n", err)
+	}
+	opentracing.SetGlobalTracer(nativeTracer)
+}
+
+func initZipkin() {
+	reporter := zipkinhttp.NewReporter("http://localhost:9411/api/v2/spans")
+	endpoint, err := zipkin.NewEndpoint("dobbugoZipkinTracingService", "myservice.mydomain.com:80")
+	if err != nil {
+		logger.Errorf("unable to create local endpoint: %+v\n", err)
+	}
+	nativeTracer, err := zipkin.NewTracer(reporter, zipkin.WithLocalEndpoint(endpoint))
+	if err != nil {
+		logger.Errorf("unable to create tracer: %+v\n", err)
+	}
+	tracer := zipkinot.Wrap(nativeTracer)
+	opentracing.SetGlobalTracer(tracer)
+}
---
+++ tracing/jsonrpc/go-server/pkg/user.go
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pkg
+
+import (
+	"context"
+	gxlog "github.com/dubbogo/gost/log"
+	"github.com/opentracing/opentracing-go"
+	"time"
+)
+
+type (
+	User struct {
+		Id    string    `json:"id"`
+		Name  string    `json:"name"`
+		Age   int       `json:"age"`
+		Birth time.Time `json:"time"`
+	}
+)
+
+type UserProvider struct {
+}
+
+func (u *UserProvider) GetUser(ctx context.Context, req []interface{}) (*User, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "User-JsonRPC-Server-Span")
+	gxlog.CInfo("req:%#v", req)
+	user := &User{Id: "001", Name: "zhangsan-dubbogo", Age: 18, Birth: time.Now()}
+	span.Finish()
+	return user, nil
+}
+
+func (u *UserProvider) Reference() string {
+	return "UserProvider"
+}
