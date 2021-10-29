3c92611645b76b4a1cda2b90bd141a7c4434ba81
LaurenceLiZhixin <382673304@qq.com>
2021-10-29 19:38:30 +0800

    fix: grpc samples

--- rpc/grpc/go-server/cmd/server.go
+++ rpc/grpc/go-server/cmd/server.go
@@ -41,7 +41,7 @@ var (
 )
 
 type GreeterProvider struct {
-	pb.UnimplementedGreeterServer
+	pb.GreeterProviderBase
 }
 
 func (g *GreeterProvider) SayHello(ctx context.Context, req *pb.HelloRequest) (reply *pb.HelloReply, err error) {
