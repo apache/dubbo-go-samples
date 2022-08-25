06509eda717ee1988fe45ba4bd1f608ba9c3aa69
chickenlj <ken.lj.hz@gmail.com>
2022-08-25 19:24:46 +0800

    update hello world java demo

--- helloworld/java-server/src/main/java/com/apache/dubbo/sample/basic/ApiProvider.java
+++ helloworld/java-server/src/main/java/com/apache/dubbo/sample/basic/ApiProvider.java
@@ -30,10 +30,15 @@ public class ApiProvider {
         service.setInterface(IGreeter.class);
         service.setRef(new IGreeter1Impl());
         service.setProtocol(new ProtocolConfig(CommonConstants.TRIPLE, 50051));
-        service.setApplication(new ApplicationConfig("demo-provider"));
-        service.setRegistry(new RegistryConfig("zookeeper://127.0.0.1:2181"));
-        service.export();
+        service.setApplication();
+        service.setRegistry();
         System.out.println("dubbo service started");
-        new CountDownLatch(1).await();
+
+         DubboBootstrap.getInstance()
+                        .application(new ApplicationConfig("demo-provider"))
+                        .registry(new RegistryConfig("zookeeper://127.0.0.1:2181"))
+                        .service(service)
+                        .start()
+                        .await();
     }
 }
