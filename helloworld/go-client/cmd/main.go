a534721eb866155bab71ad2e7bd64db29530ee93
finalt <finalt1361@163.com>
2024-01-20 17:53:13 +0800

    update helloworld proto

--- helloworld/go-client/cmd/main.go
+++ helloworld/go-client/cmd/main.go
@@ -19,10 +19,10 @@ package main
 
 import (
 	"context"
+
 	"dubbo.apache.org/dubbo-go/v3/client"
 	_ "dubbo.apache.org/dubbo-go/v3/imports"
 	greet "github.com/apache/dubbo-go-samples/helloworld/proto"
-	"github.com/apache/dubbo-go-samples/helloworld/proto/greettriple"
 	"github.com/dubbogo/gost/log/logger"
 )
 
@@ -34,7 +34,7 @@ func main() {
 		panic(err)
 	}
 
-	svc, err := greettriple.NewGreetService(cli)
+	svc, err := greet.NewGreetService(cli)
 	if err != nil {
 		panic(err)
 	}
