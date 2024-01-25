666d4753a01994363104b2702c12be946ff456c1
finalt <finalt1361@163.com>
2024-01-25 19:47:06 +0800

    add grpc server and client

---
+++ rpc/grpc/grpc-server/cmd/main.go
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"github.com/dubbogo/gost/log/logger"
+	"net"
+
+	pb "github.com/apache/dubbo-go-samples/rpc/grpc/proto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreetServiceServer
+}
+
+func (s *server) Greet(ctx context.Context, req *pb.GreetRequest) (*pb.GreetResponse, error) {
+	resp := &pb.GreetResponse{Greeting: "grpc:" + req.Name}
+	return resp, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "127.0.0.1:20001")
+	if err != nil {
+		logger.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreetServiceServer(s, &server{})
+	logger.Infof("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		logger.Fatalf("failed to serve: %v", err)
+	}
+}
