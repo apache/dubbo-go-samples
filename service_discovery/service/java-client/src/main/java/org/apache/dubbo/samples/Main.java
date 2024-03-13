11e0a549ece18740e2443287dd3c589f76e00798
yodawu <d1286360646@outlook.com>
2024-03-13 14:32:09 +0800

    update  service_discovery/service/java-client

--- service_discovery/service/java-client/src/main/java/org/apache/dubbo/samples/Main.java
+++ service_discovery/service/java-client/src/main/java/org/apache/dubbo/samples/Main.java
@@ -17,35 +17,36 @@ import java.util.concurrent.TimeUnit;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     // Define a private variable (Required in Spring)
-//    private static GreetService greetService;
+    private static GreetService greetService;
     public static void main(String[] args) throws InterruptedException, IOException {
-//            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"/spring/dubbo.consumer.xml"});
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"/spring/dubbo.consumer.xml"});
 //            context.start();
-//            greetService = (GreetService)context.getBean("greetService");
-//            GreetRequest req = GreetRequest.getDefaultInstance();
-//            GreetResponse greet = greetService.greet(req);
-//            System.out.println(greet.getGreeting());
-//            new CountDownLatch(1).await();
-        ReferenceConfig<GreetService> ref = new ReferenceConfig<>();
-        ref.setInterface(GreetService.class);
-        ref.setCheck(false);
-        ref.setProtocol(CommonConstants.TRIPLE);
-        ref.setLazy(true);
-        ref.setTimeout(100000);
-        ref.setApplication(new ApplicationConfig("demo-consumer"));
-        ref.setRegistry(new RegistryConfig("nacos://127.0.0.1:8848"));
-        final GreetService greetService = ref.get();
-
-        System.out.println("dubbo ref started");
-        GreetRequest req = GreetRequest.newBuilder().setName("laurence").build();
-        try {
-            final GreetResponse reply =greetService.greet(req);
-            TimeUnit.SECONDS.sleep(1);
-            System.out.println("Reply:" + reply);
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
-        System.in.read();
+            greetService = (GreetService)context.getBean("GreetService");
+            GreetRequest req = GreetRequest.getDefaultInstance();
+            System.out.println("dubbo ref started");
+            GreetResponse greet = greetService.greet(req);
+            System.out.println(greet.getGreeting());
+            new CountDownLatch(1).await();
+//        ReferenceConfig<GreetService> ref = new ReferenceConfig<>();
+//        ref.setInterface(GreetService.class);
+//        ref.setCheck(false);
+//        ref.setProtocol(CommonConstants.TRIPLE);
+//        ref.setLazy(true);
+//        ref.setTimeout(100000);
+//        ref.setApplication(new ApplicationConfig("dubbo-go-server"));
+//        ref.setRegistry(new RegistryConfig("nacos://127.0.0.1:8848"));
+//        final GreetService greetService = ref.get();
+//
+//        System.out.println("dubbo ref started");
+//        GreetRequest req = GreetRequest.newBuilder().setName("laurence").build();
+//        try {
+//            final GreetResponse reply =greetService.greet(req);
+//            TimeUnit.SECONDS.sleep(1);
+//            System.out.println("Reply:" + reply);
+//        } catch (Throwable t) {
+//            t.printStackTrace();
+//        }
+//        System.in.read();
     }
 
 }
