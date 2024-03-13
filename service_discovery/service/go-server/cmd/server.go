5f901977f13f7c14ad05bf5b5564b63706ec6b48
yodawu <d1286360646@outlook.com>
2024-03-13 10:20:34 +0800

    add service_discovery

---
+++ service_discovery/service/go-server/cmd/server.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+
+	"dubbo.apache.org/dubbo-go/v3"
+	// "dubbo.apache.org/dubbo-go/v3/config"
+	_ "dubbo.apache.org/dubbo-go/v3/imports"
+
+	"dubbo.apache.org/dubbo-go/v3/protocol"
+	"dubbo.apache.org/dubbo-go/v3/registry"
+
+	greet "github.com/apache/dubbo-go-samples/service_discovery/service/proto"
+	"github.com/dubbogo/gost/log/logger"
+)
+
+type GreetTripleServer struct {
+}
+
+func (svr *GreetTripleServer) Greet(ctx context.Context, req *greet.GreetRequest) (*greet.GreetResponse, error) {
+	resp := &greet.GreetResponse{Greeting: req.Name}
+	return resp, nil
+}
+
+func (s *GreetTripleServer) MethodMapper() map[string]string {
+	return map[string]string{
+		"Greet":    "greet",
+		"SayHello": "sayHello",
+	}
+}
+
+func (svr *GreetTripleServer) SayHello(ctx context.Context, req *greet.SayHelloRequest) (*greet.SayHelloResponse, error) {
+	resp := &greet.SayHelloResponse{Hello: req.Name}
+	return resp, nil
+}
+
+func main() {
+
+	ins, err := dubbo.NewInstance(
+		dubbo.WithName("dubbo-go-server"),
+		dubbo.WithRegistry(
+			registry.WithNacos(),
+			registry.WithAddress("127.0.0.1:8848"),
+			registry.WithRegisterServiceAndInterface(),
+		),
+		dubbo.WithProtocol(
+			protocol.WithDubbo(),
+			protocol.WithPort(20022),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+	srv, err := ins.NewServer()
+	if err != nil {
+		panic(err)
+	}
+	if err := greet.RegisterGreetServiceHandler(srv, &GreetTripleServer{}); err != nil {
+		panic(err)
+	}
+
+	if err := srv.Serve(); err != nil {
+		logger.Error(err)
+	}
+
+}
