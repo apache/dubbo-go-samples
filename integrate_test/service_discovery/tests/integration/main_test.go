0e032e7f0c037f404547a03d52e8154385082a70
yodawu <d1286360646@outlook.com>
2024-03-17 15:55:16 +0800

    fix: try to fix the format problem and ci problem, add a simple integreation test

--- integrate_test/service_discovery/tests/integration/main_test.go
+++ integrate_test/service_discovery/tests/integration/main_test.go
@@ -3,8 +3,27 @@ package integration
 import (
 	"os"
 	"testing"
+
+	"dubbo.apache.org/dubbo-go/v3/client"
+	_ "dubbo.apache.org/dubbo-go/v3/imports"
+	greet "github.com/apache/dubbo-go-samples/service_discovery/service/proto"
 )
 
+var greetService greet.GreetService
+
 func TestMain(m *testing.M) {
+	cli, err := client.NewClient(
+		client.WithClientURL("127.0.0.1:20000"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	greetService, err = greet.NewGreetService(cli)
+
+	if err != nil {
+		panic(err)
+	}
+
 	os.Exit(m.Run())
 }
--- integrate_test/service_discovery/tests/integration/service_discovery_test.go
+++ integrate_test/service_discovery/tests/integration/service_discovery_test.go
@@ -1,7 +1,18 @@
 package integration
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	greet "github.com/apache/dubbo-go-samples/service_discovery/service/proto"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestGreet(t *testing.T) {
-	
-}	
+	req := &greet.GreetRequest{Name: "hello world"}
+	ctx := context.Background()
+	reply, err := greetService.Greet(ctx, req)
+	assert.Nil(t, err)
+	assert.Equal(t, "hello world", reply.Greeting)
+
+}
