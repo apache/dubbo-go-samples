f6fc4ac2da216f5ef702416cfb3ae460619cbb2b
yodawu <d1286360646@outlook.com>
2024-03-17 15:43:43 +0800

    fix: try to fix the format problem and ci problem

--- service_discovery/service/java-server/src/main/java/org/apache/dubbo/samples/Main.java
+++ service_discovery/service/java-server/src/main/java/org/apache/dubbo/samples/Main.java
@@ -8,7 +8,8 @@ import java.util.concurrent.CountDownLatch;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.setProperty("dubbo.application.register-mode", "instance");
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"/spring/dubbo.server.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[] { "/spring/dubbo.server.xml" });
         context.start();
         System.out.println("dubbo service started");
         new CountDownLatch(1).await();
