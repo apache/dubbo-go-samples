14897dd0d0c4889b8e985e19fcd726d7a8a54d40
weihan <weihan@stu.ouc.edu.cn>
2024-03-27 17:16:42 +0800

    wh

--- java_interop/protobuf-triple/go/go-server/cmd/server.go
+++ java_interop/protobuf-triple/go/go-server/cmd/server.go
@@ -19,6 +19,7 @@ package main
 
 import (
 	"context"
+	"dubbo.apache.org/dubbo-go/v3"
 	_ "dubbo.apache.org/dubbo-go/v3/imports"
 	"dubbo.apache.org/dubbo-go/v3/protocol"
 	greet "github.com/apache/dubbo-go-samples/java_interop/protobuf-triple/go/proto"
