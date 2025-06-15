43afbecd611bb6bd37fba36a97a8b8dad0d28c1d
marsevilspirit <marsevilspirit@gmail.com>
2025-06-15 14:12:02 +0800

    add tls sample

---
+++ tls/server/cmd/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+
+	_ "dubbo.apache.org/dubbo-go/v3/imports"
+	"dubbo.apache.org/dubbo-go/v3/protocol"
+	"dubbo.apache.org/dubbo-go/v3/server"
+	"dubbo.apache.org/dubbo-go/v3/tls"
+	"github.com/apache/dubbo-go-samples/tls/proto"
+	"github.com/dubbogo/gost/log/logger"
+)
+
+type GreetTripleServer struct {
+}
+
+func (srv *GreetTripleServer) Greet(ctx context.Context, req *greet.GreetRequest) (*greet.GreetResponse, error) {
+	resp := &greet.GreetResponse{Greeting: req.Name}
+	return resp, nil
+}
+
+func main() {
+	srv, err := server.NewServer(
+		server.WithServerProtocol(
+			protocol.WithPort(20000),
+			protocol.WithTriple(),
+		),
+		server.WithServerTLSOption(
+			tls.WithCertFile("../../x509/server2_cert.pem"),
+			tls.WithKeyFile("../../x509/server2_key.pem"),
+			tls.WithServerName("dubbogo.test.example.com"),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := greet.RegisterGreetServiceHandler(srv, &GreetTripleServer{}); err != nil {
+		panic(err)
+	}
+
+	if err := srv.Serve(); err != nil {
+		logger.Error(err)
+	}
+}
