57ee25838dc7719d4c7194c309272bf4bf29e77f
kamtohung <kamtohung@gmail.com>
2025-08-29 23:56:26 +0800

    fix import statements in main.go to resolve ordering and grouping issues

--- compatibility/config-api/configcenter/zookeeper/go-client/cmd/client.go
+++ compatibility/config-api/configcenter/zookeeper/go-client/cmd/client.go
@@ -36,8 +36,8 @@ const configCenterZKClientConfig = `## set in config center, group is 'dubbogo',
 dubbo:
   registries:
     demoZK:
-      protocol: nacos
-      address: 127.0.0.1:8848
+      protocol: zookeeper
+      address: 127.0.0.1:2181
   consumer:
     references:
       GreeterClientImpl:
