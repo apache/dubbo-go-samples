8102c3a48db1c672b91828941c53fbb5287a5134
Oxidaner <18622412361@163.com>
2026-04-27 14:58:13 +0800

    fix: In the Java interop sample, Java -> Go fails or is incomplete in multiple sub-scenarios

--- java_interop/protobuf-triple/java-server/src/main/java/org/apache/dubbo/sample/Provider.java
+++ java_interop/protobuf-triple/java-server/src/main/java/org/apache/dubbo/sample/Provider.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
 public class Provider {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ServiceConfig<Greeter> service =  new ServiceConfig<>();
         service.setInterface(Greeter.class);
         service.setRef(new GreeterImpl());
